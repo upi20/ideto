d10afa40f3d2a3e375345f2972106a6c5dc150a1
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-07-02 00:33:39 +0700

    update scroll animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -377,4 +377,63 @@ jQuery(document).ready(function ($) {
 		}, 800);
 		return false;
 	});
+
+
+	$(document).on("scroll", onScroll);
+
+	//smoothscroll
+	$('.scroll-to-section a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+
+		$('.scroll-to-section a').each(function () {
+			$(this).removeClass('active');
+		})
+		$(this).addClass('active');
+
+		var target = this.hash,
+			menu = target;
+		var target = $(this.hash);
+		$('html, body').stop().animate({
+			scrollTop: (target.offset().top) + 1
+		}, 500, 'swing', function () {
+			window.location.hash = target;
+			$(document).on("scroll", onScroll);
+		});
+	});
+
+	// Menu elevator animation
+	$('.scroll-to-section').on('click', function () {
+		console.log("cek");
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				var width = $(window).width();
+				if (width < 991) {
+					$('.menu-trigger').removeClass('active');
+					$('.header-area .nav').slideUp(200);
+				}
+				$('html,body').animate({
+					scrollTop: (target.offset().top) + -60
+				}, 700);
+				return false;
+			}
+		}
+	});
 });
+
+function onScroll(event) {
+	var scrollPos = $(document).scrollTop();
+	$('.nav a').each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			$('.nav ul li a').removeClass("active");
+			currLink.addClass("active");
+		}
+		else {
+			currLink.removeClass("active");
+		}
+	});
+}
